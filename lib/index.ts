993920946bf7f036615c02a842981645ecb08a89
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-10 11:29:38 -0500

    conventional-commits-parser

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,11 @@
 import { readFileSync } from 'jsonfile'
 import never from 'never'
+import { sync } from 'conventional-commits-parser'
+import eslint from 'conventional-changelog-eslint'
 
+console.log(eslint)
 const eventFile = process.env.GITHUB_EVENT_PATH ?? never('No GITHUB_EVENT_PATH')
-
-console.log(readFileSync(eventFile))
+const events = readFileSync(eventFile)
+console.log(events.commits.map(({ message }) => {
+  return sync(message)
+}))
