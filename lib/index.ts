a0d6894924b8956443f1df14bc41cbf3e8809e2c
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-10 11:03:01 -0500

    Read eventFile

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,6 @@
-console.log(process.env)
+import { readFileSync } from 'jsonfile'
+import never from 'never'
+
+const eventFile = process.env.GITHUB_EVENT_PATH ?? never('No GITHUB_EVENT_PATH')
+
+console.log(readFileSync(eventFile))
